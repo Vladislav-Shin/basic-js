7dc7483c1c26052a879103439fe1459d7741c7d3
Vlad <akame678@gmail.com>
2022-04-29 02:35:10 +0500

    first commit

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function countCats(matrix) {
+  matrix = matrix.flat(Infinity);
+  return matrix.filter(item => item == "^^" ? item : false).length;
 }
+
+
 
 module.exports = {
   countCats
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,64 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if (arr[0] == "--discard-prev") {
+    arr.splice(0, 1);
+  }
+  let newArr = [];
+  let res = arr
+    .map((item, i) => {
+      if (item == "--double-next") {
+        return arr[i + 1];
+      }
+      if (item == "--double-prev") {
+        return arr[i - 1];
+      }
+      if (item == "--discard-next") {
+        return (arr[i + 1] = null);
+      }
+      if (item == "--discard-prev") {
+        return item;
+      } else {
+        return item;
+      }
+    })
+    .filter((item) => {
+      if (item !== null) {
+        return item;
+      }
+    });
+
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] !== "--discard-prev") {
+      newArr.push(res[i]);
+    } else {
+      if (arr[i - 2] == "--discard-next") {
+        newArr.pop();
+      }
+      if (arr[i - 2] == "--double-next") {
+        newArr.pop();
+      }
+    }
+  }
+  return newArr;
 }
 
 module.exports = {
-  transform
+  transform,
 };
